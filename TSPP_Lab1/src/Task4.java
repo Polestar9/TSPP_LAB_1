f61bd79cfb7fdbc2b2aedac0a85522719e3a3042
Polestar9 <89412637+Polestar9@users.noreply.github.com>
2024-05-09 22:43:02 +0300

    Update Task4.java

--- TSPP_Lab1/src/Task4.java
+++ TSPP_Lab1/src/Task4.java
@@ -7,18 +7,34 @@ public class Task4
         while (true)
         {
             System.out.print("Введіть значення змінної x: ");
-            double x = scanner.nextDouble();
+            double x = 0.0;
+            if (scanner.hasNextDouble())
+            {
+                x = scanner.nextDouble();
+            } else {
+                System.out.println("Помилка: Введено не число. Будь ласка, введіть числове значення.");
+                scanner.next(); // Очистка буфера введення
+                continue;
+            }
             System.out.print("Введіть значення змінної y: ");
-            double y = scanner.nextDouble();
+            double y = 0.0;
+            if (scanner.hasNextDouble())
+            {
+                y = scanner.nextDouble();
+            } else {
+                System.out.println("Помилка: Введено не число. Будь ласка, введіть числове значення.");
+                scanner.next(); // Очистка буфера введення
+                continue;
+            }
+
             if (!isValidInput(x, y))
             {
                 System.out.println("Помилка обчислення: Ділення на 0: знаменник дорівнює 0");
-            }
-            else
-            {
+            } else {
                 double result = calculateFunction(x, y);
                 System.out.println("Значення функції для введених x та y: " + result);
             }
+
             System.out.print("Бажаєте продовжити (y/n)? ");
             String continueInput = scanner.next();
             if (!continueInput.equalsIgnoreCase("y"))
@@ -28,11 +44,11 @@ public class Task4
         }
         scanner.close();
     }
+
     public static boolean isValidInput(double x, double y)
     {
         return (x != 0 || y != 0);
     }
-
     public static double calculateFunction(double x, double y)
     {
         return (x * x * x + x * y - y * y) / (1 + x * x + y * y);
